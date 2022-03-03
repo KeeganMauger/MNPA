3ae988b579b4a74cc034df0e6438a9c060c2e049
KeeganMauger <keeganmauger@cmail.carleton.ca>
2022-03-03 14:18:37 -0500

    CCVS creation

--- ccvs.m
+++ ccvs.m
@@ -1,7 +1,29 @@
-function [outputArg1,outputArg2] = untitled(inputArg1,inputArg2)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function ccvs(nd1,nd2,ni1,ni2,val)
+% Adds the stamp of a dependent current-controlled 
+% voltage-source(CCVS)to the matrices in circuit 
+% representation.
+%
+%   ni1 O-------|                  |----------o nd1
+%               |                  |
+%               |                 /+\
+%               |              | /   \    Vnd1-Vnd2 = val*(Ij)
+%           Ij  V           Ik | \   /
+%               |              V  \-/ 
+%               |                  |
+%   ni2 O-------|                  |----------o nd2
+%
+%  (1) "nd1 & nd2" are the nodes across the dependent
+%                  voltage source.
+%  (2) "ni1 & ni2" are the nodes corresponding to the 
+%                  controller current
+%
+%   nd1: (+) node   \
+%   nd2: (-) node   |----->  Vnd1-Vnd2 = val*(Ij)
+%   ni1: (+) node   |
+%   ni2: (-) node   /
+%   F: alpha, ohms
+%---------------------------------------------------------------
+global G b C 
+
 end
 
