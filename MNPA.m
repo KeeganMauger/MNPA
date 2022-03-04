b87ef1ea0f2c177cf5715091bf0fd11b7f1de0e7
KeeganMauger <keeganmauger@cmail.carleton.ca>
2022-03-04 11:31:15 -0500

    Normal perturbations of C

--- MNPA.m
+++ MNPA.m
@@ -1,6 +1,7 @@
 clear all
 close all
 clc
+set(0,'DefaultFigureWindowStyle','docked')
 
 % DC Case
 global G b C
@@ -65,6 +66,7 @@ hold off
 
 clear all
 clc
+set(0,'DefaultFigureWindowStyle','docked')
 % AC Case
 global G b C
 nodes = 6;
@@ -120,6 +122,65 @@ hold on
 plot(w,abs(x(3,:)))
 grid on
 saveas(gcf,'Figure2')
+hold off
+
+mag = abs(x(5,:)./x(1,:));
+gain = mag2db(mag);
+figure(3)
+plot(w,gain)
+grid on
+saveas(gcf,'Figure3')
+
+% AC Case - Normal Perturbations on C
+
+clear all
+clc
+set(0,'DefaultFigureWindowStyle','docked')
+global G b C
+nodes = 6;
+G = sparse(nodes,nodes);
+C = sparse(nodes,nodes);
+b = sparse(nodes,1);
+
+% MNA setup
+Vin = 1;
+Vprobe = 0;
+R1 = 1;
+R2 = 2;
+R3 = 10;
+R4 = 0.1;
+R5 = 1000;
+C1 = 0.25;
+L1 = 0.2;
+alpha = 100;
+
+cap(1,2,C1);
+res(1,2,R1);
+res(2,0,R2);
+res(3,6,R3);
+res(4,5,R4);
+res(5,0,R5);
+xr = vol(6,0,Vprobe);
+ind(2,3,L1);
+vol(1,0,Vin);
+vcvs(4,0,xr,0,alpha);
+
+w = pi;
+s = j*w;
+std = 0.05;
+mu = C1;
+numR = 1000;
+r = normrnd(mu,std,1,numR);
+
+C0 = zeros(width(G),length(G),numR);
+for i = 1:width(r)
+    cap(1,2,r(i));
+    C0(:,:,i) = C;
+end
+
+
+
+
 
 
 
